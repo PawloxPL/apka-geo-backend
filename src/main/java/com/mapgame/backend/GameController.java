f603e0a455928d0443b98d00af395a4328bc427c
Paweł Brzeziński <pawlobrzezinski2004@wp.pl>
2026-01-17 23:54:11 +0100

    dodanie testowego dla flag

--- src/main/java/com/mapgame/backend/GameController.java
+++ src/main/java/com/mapgame/backend/GameController.java
@@ -3,16 +3,19 @@ package com.mapgame.backend;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
-import java.util.List;
 import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
 
 class Country {
     public String name;
     public String code;
+    public String capital;
 
-    public Country(String name, String code) {
+    public Country(String name, String code, String capital) {
         this.name = name;
         this.code = code;
+        this.capital = capital;
     }
 }
 
@@ -20,13 +23,21 @@ class Country {
 @RequestMapping("/api/game")
 public class GameController {
 
-    @GetMapping("/countries")
-    public List<Country> getCountries() {
+    @GetMapping("/quiz-data") // Zmieńmy nazwę endpointu na bardziej adekwatną
+    public List<Country> getQuizData() {
         List<Country> countries = new ArrayList<>();
-        countries.add(new Country("Polska", "PL"));
-        countries.add(new Country("Niemcy", "DE"));
-        countries.add(new Country("Francja", "FR"));
-        countries.add(new Country("Włochy", "IT"));
+        // Dodaj więcej krajów, żeby losowanie miało sens!
+        countries.add(new Country("Polska", "PL", "Warszawa"));
+        countries.add(new Country("Niemcy", "DE", "Berlin"));
+        countries.add(new Country("Francja", "FR", "Paryż"));
+        countries.add(new Country("Włochy", "IT", "Rzym"));
+        countries.add(new Country("Hiszpania", "ES", "Madryt"));
+        countries.add(new Country("Portugalia", "PT", "Lizbona"));
+        countries.add(new Country("Wielka Brytania", "GB", "Londyn"));
+        countries.add(new Country("Holandia", "NL", "Amsterdam"));
+        
+        // Tasujemy listę na serwerze, żeby za każdym razem kolejność pytań była inna
+        Collections.shuffle(countries);
         
         return countries;
     }
